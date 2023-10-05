74d3b1ef5b1bde21af0295f493a58aa381736004
fxydoctor <fxydoctor@LAPTOP-BUC949NB.dhcp.wustl.edu>
2023-10-05 11:09:04 -0500

    hi

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,17 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+	StdDraw.setPenColor(255,109,182);
+	StdDraw.filledRectangle(10, 10, 10, 10);
+	StdDraw.setPenColor(0,243,234);
+	double[] array1=new double[3];
+	double[] array2=new double[3];
+	array1[0]	= 0.2;
+    array1[1] = 0.5;
+    array1[2] = 0.2;
+    array2[0]= 0.8;
+    array2[1]=0.6;
+    array2[2]=0.3;
+	StdDraw.filledPolygon(array1,array2);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,37 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+		if(isFilled==true) {
+		if(shapeType.equals("ellipse")) {
+		StdDraw.setPenColor(redComponent,greenComponent,blueComponent);	
+		StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}
+		else if(shapeType.equals("rectangle")) {
+			StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+			StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+		}else if(shapeType.equals("triangle")) {
+			double parameterFive = in.nextDouble();
+			double parameterSix = in.nextDouble();
+			double[]array1 = new double[3];
+			double[] array2 = new double[3];
+			array1[0]=parameterOne;
+			array1[1]	=	parameterThree;
+			array1[2]=parameterFive;
+			array2[0]=parameterTwo;
+			array2[1]=parameterFour;
+			array2[2]=parameterSix;
+			StdDraw.setPenColor(redComponent,greenComponent,blueComponent);
+			StdDraw.filledPolygon(array1, array2);
+		}
+	}
 	}
 }
